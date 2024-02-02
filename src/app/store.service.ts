d18bcad26a23244003adc0e2377a8e68c5f52266
mikolaj <mikolaj.guzek-szopa@caltha.eu>
2024-02-02 11:50:53 +0100

    Save to db

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -14,16 +14,27 @@ export class StoreService {
     set(ref(db, 'plan'), {
       allGuests: plan.allGuests,
       leftSqare1: plan.leftSqare1,
+      leftSqare1R: plan.leftSqare1R,
       leftSqare2: plan.leftSqare2,
+      leftSqare2R: plan.leftSqare2R,
       leftSqare3: plan.leftSqare3,
+      leftSqare3R: plan.leftSqare3R,
       leftSqare4: plan.leftSqare4,
+      leftSqare4R: plan.leftSqare4R,
       middleCircle1: plan.middleCircle1,
+      middleCircle1R: plan.middleCircle1R,
       middleCircle2: plan.middleCircle2,
+      middleCircle2R: plan.middleCircle2R,
       middleCircle3: plan.middleCircle3,
+      middleCircle3R: plan.middleCircle3R,
       rightSqare1: plan.rightSqare1,
+      rightSqare1R: plan.rightSqare1R,
       rightSqare2: plan.rightSqare2,
+      rightSqare2R: plan.rightSqare2R,
       rightSqare3: plan.rightSqare3,
-      rightSqare4: plan.rightSqare4
+      rightSqare3R: plan.rightSqare3R,
+      rightSqare4: plan.rightSqare4,
+      rightSqare4R: plan.rightSqare4R
     });
   }
 }
